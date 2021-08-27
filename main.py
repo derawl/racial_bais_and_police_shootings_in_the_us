7c37db5de69819e5a228d94491ec5d59d2319e06
JesusKid <frawllings@gmail.com>
2021-08-27 17:26:01 +0100

    main.py added new value proportionate attacks and charts(concerning deaths per state and the proportion of other races per state compared to white).

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
+import statsmodels.api as sm
 
 #Dash import
 import dash
@@ -85,14 +86,13 @@ race_of_min = deaths_by_police.loc[min_age_of_deaths]['race']
 """
 # deaths aggrgation by year
 deaths_by_police['year'] = pd.DataFrame(deaths_by_police['date'].dt.year)
-deaths_per_state = deaths_by_police.groupby('state',  as_index=False)['name'].count()
+deaths_per_state = deaths_by_police.groupby(['state'],  as_index=False)['name'].count()
 
+print(deaths_per_state)
 # deaths aggrgation by gender
 gender_disparity_of_deaths = deaths_by_police.groupby('gender')['name'].count()
 deaths_per_year = deaths_by_police.groupby(['year', 'gender'])['name'].count()
 
-print(f"Number of females deaths caused by police shootings: {gender_disparity_of_deaths['F']}")
-print(f"Number of male deaths caused by police shootings: {gender_disparity_of_deaths['M']}")
 
 # People below the poverty level per state and police shootings that resulted in deaths--------------------------
 for x in pct_people_below_poverty_level.index:
@@ -107,9 +107,9 @@ poverty_rate_state = pct_people_below_poverty_level.groupby('Geographic Area')['
 deaths_per_state_poverty_levels = pd.merge(deaths_per_state, poverty_rate_state.set_index('Geographic Area'), left_on='state', right_index=True)
 deaths_per_state_poverty_levels['death rate'] = (deaths_per_state_poverty_levels['name']/deaths_per_state_poverty_levels['name'].sum())*100
 correlation_pl = deaths_per_state_poverty_levels['poverty_rate'].corr(deaths_per_state_poverty_levels['death rate'])
-print(f'Correlation between deaths caused by police shootings and poverty'
+'''print(f'Correlation between deaths caused by police shootings and poverty'
       f'levels per state\n'
-      f'{correlation_pl}')
+      f'{correlation_pl}')'''
 ###median household income per state and its correlation with deaths per state:---------------------------------------------
 
 median_income = Median_house_hold_income
@@ -141,7 +141,7 @@ combined_median_income['rate_of_deaths'] = (combined_median_income['name'] /  co
 
 correlation = combined_median_income['rate_of_median_income'].corr(combined_median_income['rate_of_deaths'])
 #print(combined_median_income)
-print(f'Correlation between deaths caused by police shootings and median household income {correlation}')
+#print(f'Correlation between deaths caused by police shootings and median household income {correlation}')
 
 ###Highschool Completion levels and police shootings-------------------------------------------------------------------
 
@@ -158,8 +158,8 @@ hs_vs_deaths['death_rate'] = (hs_vs_deaths['name']/hs_vs_deaths['name'].sum())*1
 hs_vs_deaths['completion_rate'] = (hs_vs_deaths['percent_completed_hs']/hs_vs_deaths['percent_completed_hs'].sum())
 hs_corr = hs_vs_deaths['completion_rate'].corr(hs_vs_deaths['death_rate'])
 
-print(hs_vs_deaths)
-print(hs_corr)
+#print(hs_vs_deaths)
+#print(hs_corr)
 
 # racial disparity and racial deaths per state---------------------------------------------------------------
 r = race_proportion_per_city
@@ -194,6 +194,10 @@ proportion_of_by_state = pd.merge(p3, hispanic_people_per_state, right_on=['Geog
                                   left_on=['Geographic area'])
 
 # Getting the aggregate of each data frame
+proportion_of_by_state['total'] = proportion_of_by_state['share_white'] +  proportion_of_by_state['share_black'] + proportion_of_by_state['share_asian']+proportion_of_by_state['share_hispanic'] + proportion_of_by_state['share_native_american']
+proportion_of_by_state['share of non-white races'] = round(((proportion_of_by_state['share_black'] + proportion_of_by_state['share_asian']+proportion_of_by_state['share_hispanic'] + proportion_of_by_state['share_native_american'])/proportion_of_by_state['total'])*100)
+print(proportion_of_by_state.to_string())
+
 p_white = proportion_of_by_state['share_white'].mean()
 p_black = proportion_of_by_state['share_black'].mean()
 p_asian = proportion_of_by_state['share_asian'].mean()
@@ -232,29 +236,32 @@ race_sorted_deaths = pd.DataFrame({'Races': races, 'Population': pop_proportion,
 
 
 # visualizations and reporting
-figure = px.bar(race_sorted_deaths, color='Races', x='Races', y='Comparative proportions of population deaths')
+states = px.choropleth(deaths_per_state, title='Number of deaths per state', locations='state', color='name', hover_name="state", locationmode="USA-states", scope='usa')
+states2 = px.choropleth(proportion_of_by_state, title='Percentage of non-white races per state', locations=proportion_of_by_state.index, color='share of non-white races', hover_name='share of non-white races', locationmode="USA-states", scope='usa')
+
+figure = px.bar(race_sorted_deaths,  color='Races', x='Races', y='Comparative proportions of population deaths')
 race_composition = px.pie(race_sorted_deaths, color='Races', values='Population', names='Races', title='Percentage US racial composition')
 race_shooting = px.bar(race_sorted_deaths, x='Races', y=['Deaths'], color='Races', title='Average US racial Deaths')
 
 fig3 = px.pie(race_sorted_deaths, color='Races', values='Comparative proportions of population deaths', names='Races',
               title='Proportionate percentage of deaths caused by police shootings to racial populations in the USA')
 
-poverty = px.scatter(deaths_per_state_poverty_levels, x='poverty_rate',  y=['death rate'])
-median_income_fig = px.scatter(combined_median_income, x='rate_of_median_income',  y=['rate_of_deaths'])
-hs_fig = px.scatter(hs_vs_deaths, x='completion_rate',  y=['death_rate'])
+poverty = px.scatter(deaths_per_state_poverty_levels, x='poverty_rate',  y='death rate', trendline="ols")
+median_income_fig = px.scatter(combined_median_income, x='rate_of_median_income',  y=['rate_of_deaths'], trendline="ols")
+hs_fig = px.scatter(hs_vs_deaths, x='completion_rate',  y=['death_rate'], trendline="ols")
 attacks = px.bar(threats, x=threats.index, y=['per_other', 'per_attacks'])
 
 
 details = html.Div(children=[
     html.H3(children='Highlights'),
     html.Div(children=[
-        html.P(children=f'⚫ Average age of deaths: {int(mean_age_of_deaths)} years'),
-        html.P(children=f'⚫ Median age of deaths: {int(median_age_of_deaths)} years'),
-        html.P(children=f'⚫ Age with the highest frequency of death: {int(mode_age_of_deaths)} years'),
-        html.P(children=f'⚫ Oldest person killed by police shooting {int(max_age_of_deaths)} years, Name: {name_of_max}, Race: White'),
-        html.P(children=f'⚫ Youngest person killed by police shooting {int(min_age_of_deaths)} years, Name: {name_of_min}, Race: Hispanic'),
-        html.P(children=f"⚫ Number of female deaths caused by police shootings: {gender_disparity_of_deaths['F']} people"),
-        html.P(children=f"⚫ Number of male deaths caused by police shootings: {gender_disparity_of_deaths['M']} people"),
+        html.P(children=f'* Average age of deaths:   {int(mean_age_of_deaths)} years'),
+        html.P(children=f'* Median age of deaths:   {int(median_age_of_deaths)} years'),
+        html.P(children=f'* Age with the highest frequency of death:   {int(mode_age_of_deaths)} years'),
+        html.P(children=f'* Oldest person killed by police shooting: {name_of_max}, Age: {int(max_age_of_deaths)} years, Race: White'),
+        html.P(children=f'* Youngest person killed by police shooting: {name_of_min}, Age:   {int(min_age_of_deaths)} years, Race: Hispanic'),
+        html.P(children=f"* Number of female deaths caused by police shootings:   {gender_disparity_of_deaths['F']} people"),
+        html.P(children=f"* Number of male deaths caused by police shootings:   {gender_disparity_of_deaths['M']} people"),
 
     ])
 ], style={'width': '100%', 'position': 'static', 'left': '0%',
@@ -262,9 +269,38 @@ details = html.Div(children=[
          'top': '10%', 'border': '3px solid white', 'height':'50%', 'padding': '2rem'}
 )
 
+Geoplot = html.Div(
+   children=[
+       html.H5(children='Categorizations per State', style={'text-align':'center', 'font-weight': 'bold'}),
+       html.Div(
+           className='row',
+           children=[
+               html.Div(
+                   className='six columns',
+                   children=[
+                       dcc.Graph(
+                           id='geo',
+                           figure=states
+                       ),
+                   ]
+               ),
+               html.Div(
+                   className='six columns',
+                   children=[
+                       dcc.Graph(
+                           id='geo1',
+                           figure=states2
+                       ),
+                   ]
+               )
+           ]
+       )
+   ]
+)
+
 
 Graphs_poverty = html.Div(children=[
-    html.H3(children=['Most Disputed and how they impact police shootings']),
+    html.H3(children=['Most Disputed and how they impact police shootings'], style={'text-align':'left', 'font-weight': 'bold'}),
     html.Div(
         className="row",
         children=[
@@ -304,7 +340,14 @@ Graphs_poverty = html.Div(children=[
 
             ),
 
-           html.Div(
+        ],
+    ),
+
+    html.Div(
+        className='row',
+        children=[
+
+html.Div(
                 className="six columns",
                 children=[
                     html.H6(children='Over 25 High school completion Vs Police shootings'),
@@ -322,48 +365,76 @@ Graphs_poverty = html.Div(children=[
                 ]
 
             ),
-            html.Div(
-            className='twelve columns',
-            children=[
-            html.H5(children='Summary'),
-            html.P(children='The various variables have little effect on deaths caused by police shootings'
-                            'in the US')
-            ]
-            )
+
+           html.Div(
+                className="six columns",
+                children=[
+                    html.H6(children='Deaths resulting from attacks on the police as percentage of deaths per race'),
+                    dcc.Graph(
+                        id='Attacks',
+                        figure=attacks
+                    )
+                ],
+
+            ),
 
         ]
-)
+    ),
+
+    html.Div(
+    children=[
+    html.H5(children='Summary'),
+    html.P(children='The median income, poverty levels and high school drop out rate have little effect on deaths caused by police shootings'
+                    'in the US. However one fact has been debunked white people are the most likely'
+                    'to attack the police in a standoff compared to other races. This is closely followed'
+                    'by blacks.')
+    ]
+    )
+
+
+
 ], style={ 'display': 'block',
            'padding': '8rem'})
 
 
 #Racial deaths eplanation
 Graphs = html.Div(children=[
-       html.H5(children='The Race variable Vs Police Shootings'),
+       html.H3(children='The Race variable Vs Police Shootings', style={'text-align':'left', 'font-weight': 'bold'}),
        html.P(children='The statement that ones race influences their probaility of being shot'
                        'in a police standoff is a highly controverisial topic. However the data shows us what we need'
                        'to know.'),
-    html.Div(children=[
+
+    html.Div(
+        className='row',
+        children=[
+
+html.Div(children=[
         dcc.Graph(
+            className='six columns',
             id='race',
             figure=race_composition
         ),
 
         dcc.Graph(
+            className='six columns',
                     id='race1',
                     figure=race_shooting
                 ),
 
         html.H6(children='Summary'),
         html.P(children='This chart clearly shows us that people in the white race category are killed'
-                        'more than other categories. However, they equally makeup the majority of the total'
-                        'population in the USA so it is misleading. '
-                        'A proportionate look gives us the answer we want')
+                        ' more than other categories. However, they are less proportionately affected than other races.'
+                        ' Even though about 79.4% of the US is white, they account for 52% of deaths by police shootings while blacks on the'
+                        'other hand account for 26% of deaths despite being only 7.2% of the population'
+                        ' A proportionate look such as the one below gives a clearer insights.')
     ]),
+
+        ], style={'padding-bottom': '7rem'}
+    ),
    html.Div(children=[
 
  html.Div(
-        children=[html.H3(children=['Deaths caused by police shootings as a proportion of population per race'])]
+        children=[html.H5(children=['Deaths caused by police shootings as a proportion of population per race'])]
     ),
     html.Div(
         children=[
@@ -402,7 +473,7 @@ html.Div(
                     ' population. People in the black race category indeed have the most probability to be shot in'
                     ' a police standoff.')
 
-   ], style={'padding':'2rem', 'box-shadow': '2px 2px 2px 2px #888888', 'border-radius':'10px'})
+   ], style={'padding':'2rem', 'border-radius':'10px'})
 ], style={ 'display': 'block',
            'padding': '8rem'})
 
@@ -411,27 +482,12 @@ title = html.H1(
     style={'padding':'4rem', 'text-align':'center'}
 )
 
-attacks = html.Div(children=[
-
-    html.Div(
-        children=[
-        html.H4(children='Proportion of deaths as a result of an attack on the police'),
-        dcc.Graph(
-                id='Chartd',
-                figure=attacks
-
-            )
-        ],
-        style={'padding':'1rem 20rem 1rem 1rem'}
-    )
-
-])
-
+attacks = ''
 conclusion = html.Div(children=[
 
     html.Div(
         children=[
-        html.H2(children='Conclusion'),
+        html.H3(children='Conclusion', style={'text-align':'left', 'font-weight': 'bold'}),
         html.P('Race as a motivator for police shootings in the USA is a highly debated topic. However, looking'
                ' at the insights we have given this analysis. We can better understand what the facts really are'),
         html.P(' - That all other races except native americans are more  likely to be shoot than whites'),
@@ -442,7 +498,7 @@ conclusion = html.Div(children=[
 
         html.H5('Summary'),
         html.P('Given the above conclusions, a person race could account for a greater likely hood of being shot in a police encounter. Given'
-               ' that colored people(asians, blacks, and hispanics) are more likely to be shot when compared to white people. All other variables'
+               ' that asians, blacks, and hispanics are more likely to be shot when compared to white people. All other variables'
                ' being the same.'
                ' However, this does not denote the race as a cause but opens that up for reasonable questioning. Further social expriments'
                ' better demonstrate how police respond more discriminatory towards colored people with a greater intensity on the'
@@ -453,8 +509,8 @@ conclusion = html.Div(children=[
         style={'padding':'1rem 20rem 1rem 1rem'}
     )
 
-], style={'padding':'8rem'})
-app.layout = html.Div(id='main_div', children=[title, details, Graphs_poverty, Graphs, attacks, conclusion], style = {'background-color': '#FFFFFF', 'padding':'0',
+], style={'padding':'1rem 8rem 8rem 8rem'})
+app.layout = html.Div(id='main_div', children=[title, details, Geoplot, Graphs_poverty, Graphs, attacks, conclusion], style = {'background-color': '#FFFFFF', 'padding':'0',
          'width':'100%', 'height':'100%'}
 )
 
